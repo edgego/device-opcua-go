147f175ad2801f776598feb8c705b76c9820e0b4
edgego <80612608+edgego@users.noreply.github.com>
2021-11-15 10:44:40 +0800

    Create main.go

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+// Copyright (C) 2018 EdgeGo
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"github.com/edgexfoundry/device-opcua-go"
+	"github.com/edgexfoundry/device-opcua-go/internal/driver"
+	"github.com/edgexfoundry/device-sdk-go/v2/pkg/startup"
+)
+
+const (
+	serviceName string = "edge-device-opcua"
+)
+
+func main() {
+	sd := driver.NewProtocolDriver()
+	startup.Bootstrap(serviceName, device_opcua.Version, sd)
+}
